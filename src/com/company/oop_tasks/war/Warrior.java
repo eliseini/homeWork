b239d296d3a47a16d5ce4b481b30c7626250a35f
eliseini <dogma_8888@bk.ru>
2020-11-06 16:05:53 +0600

    HomeWork

---
+++ src/com/company/oop_tasks/war/Warrior.java
@@ -0,0 +1,14 @@
+package com.company.oop_tasks.war;
+
+
+public class Warrior {
+
+    protected int hitPoint;
+    public Warrior( int hitPoint){
+          this.hitPoint=hitPoint;
+    }
+
+
+
+    }
+
